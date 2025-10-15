03c7565549df3473d1d1ac39e250badee6838d1f
Tuandev18 <phamtuan180704@gmail.com>
2025-10-15 20:51:35 +0700

    MovieAPI

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -9,4 +9,23 @@ export const movieApi = {
       ? res.data
       : res.data.data || res.data.movies || [];
   },
+  getById: async (id) => {
+    const res = await axios.get(`${API_BASE}/${id}`);
+    return res.data?.movie || res.data || {};
+  },
+
+  getEpisodes: async (id) => {
+    const res = await axios.get(`${API_BASE}/${id}/episodes`);
+    return res.data?.episodes || [];
+  },
+
+  getComments: async (id) => {
+    const res = await axios.get(`${API_BASE}/${id}/comments`);
+    return res.data?.comments || [];
+  },
+
+  postComment: async (id, content) => {
+    const res = await axios.post(`${API_BASE}/${id}/comments`, { content });
+    return res.data;
+  },
 };
