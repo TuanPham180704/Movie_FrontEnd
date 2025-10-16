1ce43777468c497a9b3cdcbc00f881b8d08515a5
Tuandev18 <phamtuan180704@gmail.com>
2025-10-16 10:36:59 +0700

    GET  movieAPI

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -3,29 +3,19 @@ import axios from "axios";
 const API_BASE = "http://localhost:8080/api/movies";
 
 export const movieApi = {
-  getTrending: async () => {
-    const res = await axios.get(`${API_BASE}/trending`);
-    return Array.isArray(res.data)
-      ? res.data
-      : res.data.data || res.data.movies || [];
-  },
-  getById: async (id) => {
-    const res = await axios.get(`${API_BASE}/${id}`);
-    return res.data?.movie || res.data || {};
+  getAll: async (page = 1) => {
+    const res = await axios.get(`${API_BASE}?page=${page}`);
+    return res.data.movies || [];
   },
 
-  getEpisodes: async (id) => {
-    const res = await axios.get(`${API_BASE}/${id}/episodes`);
-    return res.data?.episodes || [];
-  },
 
-  getComments: async (id) => {
-    const res = await axios.get(`${API_BASE}/${id}/comments`);
-    return res.data?.comments || [];
-  },
-
-  postComment: async (id, content) => {
-    const res = await axios.post(`${API_BASE}/${id}/comments`, { content });
+  getBySlug: async (slug) => {
+    const res = await axios.get(`${API_BASE}/${slug}`);
     return res.data;
+  },
+
+  getVideoByEpisode: async (slug, episode = 1) => {
+    const res = await axios.get(`${API_BASE}/${slug}/video?episode=${episode}`);
+    return res.data.videoUrl;
   },
 };
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -39,7 +39,7 @@ export default function MovieDetail() {
       </p>
       <ReactPlayer
         url={
-          movie.video_url ||
+          movie.videoUrl ||
           "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
         }
         controls
