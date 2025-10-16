f796a41fe0b489d089fcbc5c95fb72813431d7a3
Tuandev18 <phamtuan180704@gmail.com>
2025-10-16 16:29:39 +0700

    Approuter

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/router";
+
+import AppRouter from "./routes/AppRouter";
 import "./index.css";
 
 const qc = new QueryClient();
@@ -10,7 +10,7 @@ const qc = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
-      <RouterProvider router={router} />
+      <AppRouter />
     </QueryClientProvider>
   </React.StrictMode>
 );
